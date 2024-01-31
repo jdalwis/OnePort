bc779d7db16658f31672e3181123da066fb4f9e5
Janith D’Alwis <janithdalwis@MacBook-Pro-54.local>
2024-01-30 23:27:00 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("getStarted").addEventListener("click", function () {
+  alert("Thank you for your interest in OnePort Data!");
+});
+
+document
+  .getElementById("getStartedHero")
+  .addEventListener("click", function () {
+    alert("Thank you for your interest in OnePort Data!");
+  });
