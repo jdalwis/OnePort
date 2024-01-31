300f577ae446facfc2d68a2f8c93475d4c8eba4f
Janith D’Alwis <janithdalwis@MacBook-Pro-54.local>
2024-01-31 02:13:51 -0500

    Updated Website

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("getStarted").addEventListener("click", function () {
-  alert("Thank you for your interest in OnePort Data!");
-});
-
-document
-  .getElementById("getStartedHero")
-  .addEventListener("click", function () {
-    alert("Thank you for your interest in OnePort Data!");
-  });
